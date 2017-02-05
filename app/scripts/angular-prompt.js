3a8723978fde311f77c54cfc2b26ba0889991398
William Buddenberg <wabudd1@gmail.com>
2017-02-04 20:19:27 -0500

    Swap templatecache for real html
    Deleted $templateCache in angular-prompt.js and replaced it with the
    separate file (easier to make changes this way)

--- app/scripts/angular-prompt.js
+++ app/scripts/angular-prompt.js
@@ -11,7 +11,7 @@ angular.module('cgPrompt').factory('prompt',['$uibModal','$q',function($uibModal
             label: '',
             value: '',
             values: false,
-            buttons: [                
+            buttons: [
                 {label:'OK',primary:true},
                 {label:'Cancel',cancel:true}
             ]
@@ -30,7 +30,7 @@ angular.module('cgPrompt').factory('prompt',['$uibModal','$q',function($uibModal
         var defer = $q.defer();
 
         $uibModal.open({
-            templateUrl:'angular-prompt.html',
+            templateUrl:'scripts/angular-prompt.html',
             controller: 'cgPromptCtrl',
             resolve: {
                 options:function(){
@@ -98,47 +98,5 @@ angular.module('cgPrompt').controller('cgPromptCtrl',['$scope','options','$timeo
         }
     },100);
 
-
-}]);
-
-
-angular.module('cgPrompt').run(['$templateCache', function($templateCache) {
-  'use strict';
-
-  $templateCache.put('angular-prompt.html',
-    "<div>\n" +
-    "    <div class=\"modal-header\">\n" +
-    "        <button type=\"button\" class=\"close pull-right\" ng-click=\"$dismiss()\" aria-hidden=\"true\">×</button>\n" +
-    "        <h4 class=\"modal-title\">{{options.title}}</h4>\n" +
-    "    </div>\n" +
-    "    <div class=\"modal-body\">\n" +
-    "\n" +
-    "        <p ng-if=\"options.message\">\n" +
-    "            {{options.message}}\n" +
-    "        </p>\n" +
-    "\n" +
-    "        <form id=\"cgPromptForm\" name=\"form.cgPromptForm\" ng-if=\"options.input\" ng-submit=\"submit()\">\n" +
-    "            <div class=\"form-group\" ng-class=\"{'has-error':cgPromptForm.$invalid && changed}\">\n" +
-    "                <label for=\"cgPromptInput\">{{options.label}}</label>\n" +
-    "                <input id=\"cgPromptInput\" type=\"text\" class=\"form-control\"  placeholder=\"{{options.label}}\" ng-model=\"input.name\" required ng-change=\"changed=true\" ng-if=\"!options.values || options.values.length === 0\"/ autofocus=\"autofocus\">\n" +
-    "                <div class=\"input-group\" ng-if=\"options.values\">\n" +
-    "                    <input id=\"cgPromptInput\" type=\"text\" class=\"form-control\" placeholder=\"{{options.label}}\" ng-model=\"input.name\" required ng-change=\"changed=true\" autofocus=\"autofocus\"/>\n" +
-    "\n" +
-    "                    <div class=\"input-group-btn\" dropdown>\n" +
-    "                        <button type=\"button\" class=\"btn btn-default dropdown-toggle\" dropdown-toggle data-toggle=\"dropdown\"><span class=\"caret\"></span></button>\n" +
-    "                        <ul class=\"dropdown-menu pull-right\">\n" +
-    "                            <li ng-repeat=\"value in options.values\"><a href=\"\" ng-click=\"input.name = value\">{{value}}</a></li>\n" +
-    "                        </ul>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "         </form>\n" +
-    "\n" +
-    "    </div>\n" +
-    "    <div class=\"modal-footer\">\n" +
-    "        <button ng-repeat=\"button in options.buttons track by button.label\" class=\"btn btn-default {{button.class}}\" ng-class=\"{'btn-primary':button.primary}\" ng-click=\"buttonClicked(button)\">{{button.label}}</button>\n" +
-    "    </div>\n" +
-    "</div>"
-  );
 
 }]);
